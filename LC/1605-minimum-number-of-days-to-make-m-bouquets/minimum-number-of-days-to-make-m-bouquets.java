9c9d4ceb628836065d919e7e21d2d28467e7ef86
Karthikeya Tummala <89243957+karthikeya-tummala@users.noreply.github.com>
2025-07-14 00:07:54 +0530

    Time: 22 ms (29.76%) | Memory: 58.1 MB (64.96%) - LeetSync

---
+++ LC/1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] lowHigh(int[] arr) {
+        int[] ans = {Integer.MAX_VALUE, -1};
+        for(int i = 0; i < arr.length; i++) {
+            if (arr[i] < ans[0]) ans[0] = arr[i];
+            if (arr[i] > ans[1]) ans[1] = arr[i];
+        }
+        return ans;
+    }
+
+    public int number(int[] arr, int day, int flowers) {
+        int count = 0;
+        int number = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] <= day) {
+                count++;
+            }
+            else {
+                number += count / flowers;
+                count = 0;
+            }
+        }
+        number += count / flowers;
+        return number;
+    }
+
+    public int minDays(int[] bloomDay, int m, int k) {
+        if ((long) m * k > bloomDay.length) return -1;
+        int[] par = lowHigh(bloomDay);
+        int low = par[0], high = par[1];
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int bouques = number(bloomDay, mid, k);
+            if (bouques >= m) high = mid - 1;
+            else low = mid + 1;
+        }
+        return low;
+    }
+}
