311f76171888d349eab1151a8384f43684f0d687
Karthikeya Tummala <89243957+karthikeya-tummala@users.noreply.github.com>
2025-07-14 00:13:39 +0530

    Time: 19 ms (57.67%) | Memory: 58.8 MB (20.16%) - LeetSync

--- LC/1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ LC/1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,37 +1,28 @@
 class Solution {
-    public int[] lowHigh(int[] arr) {
-        int[] ans = {Integer.MAX_VALUE, -1};
-        for(int i = 0; i < arr.length; i++) {
-            if (arr[i] < ans[0]) ans[0] = arr[i];
-            if (arr[i] > ans[1]) ans[1] = arr[i];
-        }
-        return ans;
-    }
-
-    public int number(int[] arr, int day, int flowers) {
-        int count = 0;
-        int number = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] <= day) {
-                count++;
-            }
-            else {
-                number += count / flowers;
-                count = 0;
-            }
-        }
-        number += count / flowers;
-        return number;
-    }
-
     public int minDays(int[] bloomDay, int m, int k) {
         if (m > bloomDay.length / k) return -1;
-        int[] par = lowHigh(bloomDay);
-        int low = par[0], high = par[1];
+        int low = Integer.MAX_VALUE, high = Integer.MIN_VALUE;
+        for (int val : bloomDay) {
+            if (val < low) low = val;
+            if (val > high) high = val;
+        }
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            int bouques = number(bloomDay, mid, k);
-            if (bouques >= m) high = mid - 1;
+            int count = 0, bouquets = 0;
+
+            for (int val : bloomDay) {
+                if (val <= mid) {
+                    count++;
+                    if (count == k) {
+                        bouquets++;
+                        count = 0;
+                    }
+                } else {
+                    count = 0;
+                }
+            }
+
+            if (bouquets >= m) high = mid - 1;
             else low = mid + 1;
         }
         return low;
