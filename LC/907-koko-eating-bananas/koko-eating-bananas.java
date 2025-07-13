1770f2fe41502367efd7b56b352347c59665c959
Karthikeya Tummala <89243957+karthikeya-tummala@users.noreply.github.com>
2025-07-13 23:22:21 +0530

    Time: 26 ms (25.72%) | Memory: 45.3 MB (34.63%) - LeetSync

---
+++ LC/907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,27 @@
+class Solution {
+    public static int findHours (int[] arr, double speed) {
+        int time = 0;
+        for (int i = 0; i < arr.length; i++) {
+            time += Math.ceil(arr[i] / speed);
+        }
+        return time;
+    }
+
+    public static int max(int[] arr) {
+        int ans = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > ans) ans = arr[i];
+        }
+        return ans;
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        int low = 1, high = Solution.max(piles);
+        while (high >= low) {
+            int mid = low + (high - low) / 2;
+            int time = findHours(piles, mid);
+            if (time <= h) high = mid - 1;
+            else low = mid + 1;
+        }
+        return low;
+    }
+}
