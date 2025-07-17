e8dbd8d0d4b80b40d60b0dc387a20687e4f75a2a
Karthikeya Tummala <89243957+karthikeya-tummala@users.noreply.github.com>
2025-07-17 10:11:08 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (10.39%) - LeetSync

--- LC/50-powx-n/powx-n.java
+++ LC/50-powx-n/powx-n.java
@@ -1,5 +1,16 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        if (n == 0) return 1;
+        double res = 1;
+        long N = Math.abs((long) n);
+        while (N > 0) {
+            if (N % 2 != 0) {
+                res *= x;
+            }
+                x *= x;
+                N /= 2;
+        }
+        if (n > 0) return res;
+        return 1 / res;    
     }
 }
